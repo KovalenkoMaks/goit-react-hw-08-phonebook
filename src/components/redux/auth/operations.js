7d27f6737a05194a9ceb411a5cbace479025c0fd
KovalenkoMaks <kovalenko.m.s1990@gmail.com>
2023-06-09 01:49:02 +0300

    Change auth url

--- src/components/redux/auth/operations.js
+++ src/components/redux/auth/operations.js
@@ -2,10 +2,8 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 import swal from 'sweetalert';
 
-// axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
-
-// axios.defaults.baseURL = 'https://goit-node-hw-restapi.onrender.com/api/';
-axios.defaults.baseURL = 'http://localhost:3000/api';
+axios.defaults.baseURL = 'https://goit-node-hw-restapi.onrender.com/api/';
+// axios.defaults.baseURL = 'http://localhost:3000/api';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
